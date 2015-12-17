28baec84ca940a10d854e5075969e2890324d578
jgladch <jeffgladchun@gmail.com>
2015-12-17 13:18:27 -0800

    Add prompt for resetting board or inputing fen string

---
+++ util/util.js
@@ -0,0 +1,23 @@
+module.exports.createTimedCommand = (command, time, self) => {
+  return () => {
+    self.engine.send(command, () => {
+      self.engine.send('stop', () => {
+        self.basePrompt();
+      });
+    });
+
+    if (time) {
+      setTimeout(() => {
+        self.engine.send('stop');
+      }, time);
+    }
+  }
+};
+
+module.exports.safeFen = (string) => {
+  if (string.indexOf('fen ') !== 0) {
+    return `fen ${string}`;
+  } else {
+    return string;
+  }
+};
